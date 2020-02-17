7b36869d87e5ee9d3640b34fd272a79ea01690ef
Alex Recker <alex@reckerfamily.com>
2020-02-17 17:32:06 -0600

    Delete wallpaper on model delete

---
+++ db/signals.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+from django.conf import settings
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from .models import Wallpaper
+
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_delete, sender=Wallpaper)
+def cleanup_wallpaper_image(sender, instance, *args, **kwargs):
+    target = os.path.join(settings.MEDIA_ROOT, instance.image.path)
+    if os.path.exists(target):
+        os.remove(target)
+        logger.info('%s triggered media cleanup, deleted %s', instance, target)
+    else:
+        logger.info('skipping media cleanup for %s, %s does not exist', instance, target)
